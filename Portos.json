fee964ba5818c035509089b8c51ece21809cbcf3
SIGMAPS <80605470+SIGMAPS@users.noreply.github.com>
2022-02-07 18:24:37 +0000

    Add files via upload

---
+++ Portos.json
@@ -0,0 +1,31 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "NomePorto": "Porto"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -8.624267578125,
+          41.154876361984655
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "NomePorto": "Lisboa"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -9.140625,
+          38.71551876930462
+        ]
+      }
+    }
+  ]
+}
---
+++ script.js
@@ -0,0 +1,88 @@
+//toggle for menus
+$(document).ready(function(){
+	$(".sub-btn").click(function(){
+		$(this).next(".sub-menu").slideToggle();
+	});
+});
+
+//javascript for the responsive navigation menu
+var menu = document.querySelector(".menu");
+var menuBtn = document.querySelector(".menu-btn");
+var closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", () => {
+	menu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+	menu.classList.remove("active");
+});
+
+//map options
+let mapOptions = {
+	center: [42.860, 3.472],
+	zoom:6,
+	zoomControl: false
+}
+
+//create map
+var map = L.map('map',mapOptions)
+
+let layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png');
+layer.addTo(map);
+
+//create layer para os Portos
+var Pportos = $.getJSON("Portos.json");
+Pportos.then(function(data){
+	var Porto = L.geoJson(data, {
+		filter: function(feature, layer){
+				return feature.properties.NomePorto == "Porto"
+		}
+	});
+
+	var Lisboa = L.geoJson(data, {
+		filter: function(feature, layer){
+				return feature.properties.NomePorto == "Lisboa"
+		}
+	});
+
+//create route for viagens
+for (var i = 0, latlngs = [], len = route.length; i < len; i++) {
+			latlngs.push(new L.LatLng(route[i][0], route[i][1]));
+		}
+
+var path = L.polyline(latlngs);
+map.addLayer(L.marker(latlngs[0]));
+map.addLayer(L.marker(latlngs[len - 1]));
+map.addLayer(path);
+path.bindPopup("Hello world");	
+
+function snake() {
+			path.snakeIn();
+		}
+
+		path.on('snakestart snake snakeend', function(ev){
+			console.log(ev.type);
+		});
+
+
+//toggle for Layer Portos
+$(".Menu-Porto").click(function(){
+	map.addLayer(Porto)
+	map.removeLayer(Lisboa)
+});
+
+$(".Menu-Lisboa").click(function(){
+	map.addLayer(Lisboa)
+	map.removeLayer(Porto)
+});
+
+
+
+//
+});
+
+
+
+
+
